6a5fbf8b573de2c9736ac03da50c59112c3eab12
whatsmysuggestion <whatsmysuggestion@gmail.com>
2021-08-07 22:53:38 +0530

    ngFor-Directive-To-Traverse-JSON-Using-Angular

---
+++ src/app/app.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+ List: any[] = 
+ [
+  {name:'a',no:1},
+  {name:'b',no:2}
+ ];	
+ 
+ 
+ 
+}
